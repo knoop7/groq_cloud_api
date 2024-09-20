c35691284eeb434a71b2535982d63cd46fad9338
KNOOP <knuop@icloud.com>
2024-09-20 12:04:46 +0800

    Update const.py

--- custom_components/groq_cloud_api/const.py
+++ custom_components/groq_cloud_api/const.py
@@ -5,7 +5,7 @@ DOMAIN = "groq_cloud_api"
 
 LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "Groq Cloud API"
+DEFAULT_NAME = "Groq Cloud"
 CONF_RECOMMENDED = "recommended"
 CONF_PROMPT = "prompt"
 CONF_CHAT_MODEL = "chat_model"
